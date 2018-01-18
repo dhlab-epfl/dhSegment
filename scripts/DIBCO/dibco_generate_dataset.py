8d50e74460e797bc66c783eaeb670723f362dbe3
soliveir <sofia.oliveiraares@epfl.ch>
2018-01-18 11:55:44 +0100

    updated dibco dataset generation with train/val splitting (among other things)

--- scripts/DIBCO/dibco_generate_dataset.py
+++ scripts/DIBCO/dibco_generate_dataset.py
@@ -6,56 +6,64 @@ from glob import glob
 from scipy.misc import imread, imsave
 import numpy as np
 from tqdm import tqdm
-import cv2
 import argparse
+from typing import List
+
+RANDOM_SEED = 0
+np.random.seed(RANDOM_SEED)
 
 
-TARGET_HEIGHT = 800
-
-
-def get_img_filenames(directory):
+def get_img_filenames(directory: str) -> List[str]:
     directory = os.path.abspath(directory)
     year = int(directory.split(os.path.sep)[-1].split('-')[0][-4:])
-    if year == 2011 or year == 2012 or year == 2014:
-        extension = '*.png'
-    elif year == 2013:
-        extension = '*.bmp'
+    if year == 2010:
+        extension = ['*.jpg', '*.bmp', '*.tif']
+    elif year == 2011 or year == 2012 or year == 2014:
+        extension = ['*.png']
+    elif year == 2013 or year == 2009:
+        extension = ['*.bmp']
     elif year == 2016:
-        extension = '*[!_gt].bmp'
+        extension = ['*[!_gt].bmp']
     else:
         raise NotImplementedError
 
-    return glob(os.path.join(directory, extension))
+    files = [glob(os.path.join(directory, ext)) for ext in extension]
+    return [item for l in files for item in l]
 
 
-def get_gt_filename(img_filename):
+def get_gt_filename(img_filename: str) -> str:
     directory, basename = os.path.os.path.split(img_filename)
     year = int(directory.split(os.path.sep)[-1].split('-')[0][-4:])
-    if year == 2011:
-        extension = 'GT.tiff'
+    if year == 2009:
+        extension = '.tiff'
+    elif year == 2011:
+        extension = '_GT.tiff'
     elif year == 2012:
-        extension = 'GT.tif'
-    elif year == 2013 or year == 2014:
-        extension = 'estGT.tiff'
+        extension = '_GT.tif'
+    elif year == 2013 or year == 2014 or year == 2010:
+        extension = '_estGT.tiff'
     elif year == 2016:
-        extension = 'gt.bmp'
+        extension = '_gt.bmp'
     else:
         raise NotImplementedError
 
-    return os.path.join(directory, '{}_{}'.format(basename.split('.')[0], extension))
+    return os.path.join(directory, '{}{}'.format(basename.split('.')[0], extension))
 
 
-def save_and_resize(img, filename, nearest=False):
-    resized = cv2.resize(img, ((img.shape[1]*TARGET_HEIGHT)//img.shape[0], TARGET_HEIGHT),
-                         interpolation=cv2.INTER_NEAREST if nearest else cv2.INTER_LINEAR)
-    imsave(filename, resized)
-
-
-def get_exported_image_basename(image_filename):
+def get_exported_image_basename(image_filename: str) -> str:
     # Get acronym followed by name of file
     directory, basename = os.path.split(image_filename)
     acronym = directory.split(os.path.sep)[-1].split('-')[0]
     return '{}_{}'.format(acronym, basename.split('.')[0])
+
+
+def generate_one_tuple(img_filename: str, output_dir: str) -> None:
+    image = imread(img_filename, mode='RGB')
+    basename_export = get_exported_image_basename(img_filename)
+    imsave(os.path.join(output_dir, 'images', '{}.jpg'.format(basename_export)), image)
+    gt = np.zeros_like(image)
+    gt[:, :, 0] = ~imread(get_gt_filename(img_filename), mode='L')
+    imsave(os.path.join(output_dir, 'labels', '{}.png'.format(basename_export)), gt)
 
 
 if __name__ == '__main__':
@@ -64,22 +72,40 @@ if __name__ == '__main__':
                         help='Input directory containing images and PAGE files')
     parser.add_argument('-o', '--output_dir', required=True, type=str, default=None,
                         help='Output directory to save images and labels')
-    parser.add_argument('-th', '--target_height', type=int, default=800,
-                        help='The desired output image height (in px)')
+    parser.add_argument('-s', '--split', action='store_true',  # default : false
+                        help='Split inputs into training and validation set')
     args = vars(parser.parse_args())
-
-    os.makedirs(os.path.join(args.get('output_dir'), 'images'), exist_ok=True)
-    os.makedirs(os.path.join(args.get('output_dir'), 'labels'), exist_ok=True)
 
     img_filenames = get_img_filenames(args.get('input_dir'))
 
-    for img_filename in tqdm(img_filenames):
-        image = imread(img_filename)
-        basename_export = get_exported_image_basename(img_filename)
-        save_and_resize(image,
-                        os.path.join(args.get('output_dir'), 'images', '{}.jpg'.format(basename_export)))
-        gt = np.zeros_like(image)
-        gt[:, :, 0] = ~imread(get_gt_filename(img_filename), mode='L')
-        save_and_resize(gt,
-                        os.path.join(args.get('output_dir'), 'labels', '{}.png'.format(basename_export)),
-                        nearest=True)
+    if args.get('split'):
+        # Split data into training and validation set (0.9/0.1)
+        train_inds = np.random.choice(len(img_filenames), size=int(0.9 * len(img_filenames)), replace=False)
+        train_mask = np.zeros(len(img_filenames), dtype=np.bool_)
+        train_mask[train_inds] = 1
+        image_filenames_train = np.array(img_filenames)[train_mask]
+        image_filenames_eval = np.array(img_filenames)[~train_mask]
+
+        # Training set
+        root_train_dir = os.path.join(args.get('output_dir'), 'train')
+        os.makedirs(os.path.join(root_train_dir, 'images'), exist_ok=True)
+        os.makedirs(os.path.join(root_train_dir, 'labels'), exist_ok=True)
+        for img_filename in tqdm(image_filenames_train):
+            generate_one_tuple(img_filename, root_train_dir)
+
+        # Validation set
+        root_val_dir = os.path.join(args.get('output_dir'), 'validation')
+        os.makedirs(os.path.join(root_val_dir, 'images'), exist_ok=True)
+        os.makedirs(os.path.join(root_val_dir, 'labels'), exist_ok=True)
+        for img_filename in tqdm(image_filenames_eval):
+            generate_one_tuple(img_filename, root_val_dir)
+    else:
+        root_test_dir = os.path.join(args.get('output_dir'), 'test')
+        os.makedirs(os.path.join(root_test_dir, 'images'), exist_ok=True)
+        os.makedirs(os.path.join(root_test_dir, 'labels'), exist_ok=True)
+        for img_filename in tqdm(img_filenames):
+            generate_one_tuple(img_filename, root_test_dir)
+
+    # Class file
+    classes = np.stack([(0, 0, 0), (255, 0, 0)])
+    np.savetxt(os.path.join(args.get('output_dir'), 'classes.txt'), classes, fmt='%d')
