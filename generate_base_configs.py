4b7cf2cca67a1c252002bea1a05bba89f9b27e73
soliveir <sofia.oliveiraares@epfl.ch>
2018-03-16 08:44:53 +0100

    updated configs

--- generate_base_configs.py
+++ generate_base_configs.py
@@ -3,15 +3,11 @@ import os
 import argparse
 import copy
 
-params_model_selected_levels = [
-    #[True, True, True, True, True],
-    [True, True, True, True, True]
-]
-
-params_training_size = [int(90e4), int(60e4)]
-# params_training_make_patches = [True]
-params_training_learning_rate = [1e-4,
-                                 1e-3]
+params_training_size = [int(100e4), int(60e4)]
+# params_training_size = [-1]
+# params_training_learning_rate = [1e-4,
+#                                  1e-3]
+params_training_focal_loss_gamma = [0.0, 1.0]
 
 
 def deep_update(destination, source):
@@ -44,22 +40,23 @@ if __name__ == '__main__':
             deep_update(base_config, json.load(f))  # type: dict
 
     dic_params = copy.deepcopy(base_config)
-    for sel_levels in params_model_selected_levels:
-        # dic_params['model_params'] = dict()
-        dic_params['model_params']['selected_levels_upscaling'] = sel_levels
-        # for upscale in params_model_upsale_params:
-        #     dic_params['model_params']['upscale_params'] = upscale
-        for size in params_training_size:
-            dic_params['training_params']['input_resized_size'] = size
-            # for mp in params_training_make_patches:
-            #     dic_params['training_params']['make_patches'] = mp
-            for learing_rate in params_training_learning_rate:
-                dic_params['training_params']['learning_rate'] = learing_rate
+    # for sel_levels in params_model_selected_levels:
+    #     dic_params['model_params'] = dict()
+    #     dic_params['model_params']['selected_levels_upscaling'] = sel_levels
+    #     for upscale in params_model_upsale_params:
+    #         dic_params['model_params']['upscale_params'] = upscale
+    for size in params_training_size:
+        dic_params['training_params']['input_resized_size'] = size
+        for gamma in params_training_focal_loss_gamma:
+            dic_params['training_params']['focal_loss_gamma'] = gamma
+        # for learing_rate in params_training_learning_rate:
+        #     dic_params['training_params']['learning_rate'] = learing_rate
 
-                dic_params['model_output_dir'] = os.path.join(args.get('model_output_dir'), 'exp_{:03d}'.format(id))
+            dic_params['model_output_dir'] = os.path.join(args.get('model_output_dir'),
+                                                          'exp_{:03d}_{}'.format(id, args.get('task')))
 
-                with open(os.path.join(args.get('config_output_dir'),
-                                       '{}_variant_{:03d}.json'.format(args.get('task'), id)), 'w') as f:
-                    json.dump(dic_params, f)
-                id += 1
+            with open(os.path.join(args.get('config_output_dir'),
+                                   '{}_variant_{:03d}.json'.format(args.get('task'), id)), 'w') as f:
+                json.dump(dic_params, f)
+            id += 1
     print('Generated {} configs'.format(id))
