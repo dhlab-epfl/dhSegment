d5a806d69b3ed361db64250cb52c3167ddc3db39
soliveir <sofia.oliveiraares@epfl.ch>
2018-03-16 08:44:08 +0100

    page : export txt with corner coordinates

--- doc_seg/evaluation/extraction.py
+++ doc_seg/evaluation/extraction.py
@@ -79,6 +79,13 @@ def cini_evaluate_folder(output_folder: str, validation_dir: str, verbose=False,
     return result
 
 
+def format_quad_to_string(quad):
+    s = ''
+    for corner in quad:
+        s += '{},{},'.format(corner[0], corner[1])
+    return s[:-1]
+
+
 def page_evaluate_folder(output_folder: str, validation_dir: str, pixel_wise: bool=True,
                          debug_folder: str=None, verbose: bool=False) -> dict:
     """
@@ -96,6 +103,7 @@ def page_evaluate_folder(output_folder: str, validation_dir: str, pixel_wise: bo
     filenames_binary_masks = glob(os.path.join(output_folder, '*.png'))
 
     global_metrics = Metrics()
+    list_boxes = list()
     for filename in tqdm(filenames_binary_masks, desc='Evaluation'):
         basename = os.path.basename(filename).split('.')[0]
 
@@ -115,15 +123,22 @@ def page_evaluate_folder(output_folder: str, validation_dir: str, pixel_wise: bo
             global_metrics += metric
 
         pred_box = find_box(np.uint8(bin_upscaled), mode='quadrilateral')
-        label_box = find_box(np.uint8(label_image), mode='quadrilateral')
+        label_box = find_box(np.uint8(label_image), mode='quadrilateral', min_area=0.0)
 
         if debug_folder is not None:
             imsave(os.path.join(debug_folder, '{}_bin.png'.format(basename)), np.uint8(bin_upscaled*255))
             orig_img = imread(os.path.join(validation_dir, 'images', '{}.jpg'.format(basename)))
-            cv2.polylines(orig_img, [label_box[:, None, :]], True, (0, 255, 0), thickness=15)
+            if label_box is not None:
+                cv2.polylines(orig_img, [label_box[:, None, :]], True, (0, 255, 0), thickness=15)
+            else:
+                print('There is no labelled page in {}'.format(basename))
             if pred_box is not None:
                 cv2.polylines(orig_img, [pred_box[:, None, :]], True, (0, 0, 255), thickness=15)
+            else:
+                print('No box found in {}'.format(basename))
             imsave(os.path.join(debug_folder, '{}_boxes.jpg'.format(basename)), orig_img)
+
+            list_boxes.append((basename, pred_box))
 
         def intersection_over_union(cnt1, cnt2):
             mask1 = np.zeros_like(label_image)
@@ -131,13 +146,20 @@ def page_evaluate_folder(output_folder: str, validation_dir: str, pixel_wise: bo
             mask2 = np.zeros_like(label_image)
             mask2 = cv2.fillConvexPoly(mask2, cnt2.astype(np.int32), 1).astype(np.int8)
             return np.sum(mask1 & mask2) / np.sum(mask1 | mask2)
-        if pred_box is not None:
+        if pred_box is not None and label_box is not None:
             iou = intersection_over_union(label_box[:, None, :], pred_box[:, None, :])
             global_metrics.IOU_list.append(iou)
         else:
             global_metrics.IOU_list.append(0)
             if verbose:
                 print('No box found for {}'.format(basename))
+
+    # TODO export filename : 4 corners to txt file
+    if debug_folder:
+        with open(os.path.join(debug_folder, 'predicted_boxes.txt'), 'w') as f:
+            for b in list_boxes:
+                s = '{},{}\n'.format(b[0], format_quad_to_string(b))
+                f.write(s)
 
     if pixel_wise:
         # global_metrics.compute_mse()
