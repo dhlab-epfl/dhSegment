67f7d503163277f2997156d2aee8c494b8a0d30e
soliveir <sofia.oliveiraares@epfl.ch>
2018-04-03 13:47:27 +0200

    demo ongoing

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ Available pretrained implementations :
 
 ### Usage
 #### Training
-* You need to have your training data in a folder containing `images` folder and `labels` folder. The pairs (images, labels) need to have the same name (it is however not mandatory to have the same extension file, however we recommend having the label images as `.png` files). 
+* You need to have your training data in a folder containing `images` folder and `labels` folder. The pairs (images, labels) need to have the same name (it is not mandatory to have the same extension file, however we recommend having the label images as `.png` files). 
 * The annotated images in `label` folder are (usually) RGB images with the regions to segment annotated with a specific color
 * The file containing the classes has the format show below, where each row corresponds to one class (including 'negative' or 'background' class) and each row has 3 values for the 3 RGB values. Of course each class needs to have a different code.
 ``` class.txt
@@ -19,10 +19,7 @@ Available pretrained implementations :
 ```
 * There are 3 types of segmentation possible:
   * `CLASSIFICATION` : Segments assigning a class to each pixel of the image
-  * `REGRESSION`: Segments using a regression
-  * `MULTILABEL` : Segments with the possibility of one pixel belonging to multiple classes (so far only tested with 2 classes)
-* It is possible to choose the number of 'up-poolings' and the layers to use in the decoding network. Use `[pretrained_model_name]_selected_levels_upscaling` in `Params` initialization.
-
+ 
 
 
 ## Method
@@ -32,8 +29,18 @@ Available pretrained implementations :
 * Model Selection : `python model_selection.py -e <experiment-folders> -m <metric-to-be-used-for-selection>`
 
 
-
 * Generate configs : `python generate_base_configs.py -co /home/docseg_models/generated_configs/ -mo /home/docseg_models/DIBCO/ -c configs/DIBCO/dibco_binarization_config.json configs/DIBCO/dibco_variant2.json`
 * Train them : `python worker.py -c /home/docseg_models/generated_configs -f /home/docseg_models/failed_configs`
 * Evaluate on validation : `python post_process_evaluation.py -m /home/docseg_models/DIBCO/* -p configs/DIBCO/dibco_post_processing_configs.json -t dibco`
 * Find best on validation and apply to test : `python model_selection.py -e /home/docseg_models/DIBCO/*/post_processing/* -m f_measure`
+
+## Demo
+[`sacred`](https://sacred.readthedocs.io/en/latest/quickstart.html) package is used to deal with experiments and trainings.
+
+1. Get the data from [link]()
+2. You can train the model from scratch with 
+    `python train.py with demo/demo_config.json`
+    or skip this step and use directly the provided model
+3. 
+
+
--- demo.py
+++ demo.py
@@ -1,8 +1,134 @@
 #!/usr/bin/env python
 
-# Load model
+import tensorflow as tf
+from dh_segment.loader import LoadedModel
+from dh_segment.post_processing import boxes_detection
+from tqdm import tqdm
+from glob import glob
+import numpy as np
+import os
+import cv2
+import argparse
+import tempfile
+from scipy.misc import imread, imsave
 
-# Postprocess
+
+def predict_on_set(filenames_to_predict, model_dir, output_dir):
+    """
+    Given a list of filenames, loads the model and saves the probabilities into a .npy file
+    :param filenames_to_predict:
+    :param model_dir:
+    :param output_dir:
+    :return:
+    """
+    with tf.Session():
+        m = LoadedModel(model_dir, 'filename')
+        for filename in tqdm(filenames_to_predict, desc='Prediction'):
+            pred = m.predict(filename)['probs'][0]
+            np.save(os.path.join(output_dir, os.path.basename(filename).split('.')[0]),
+                    np.uint8(255 * pred))
+
+
+def page_post_processing_fn(probs: np.ndarray, threshold: float=0.5, ksize_open: tuple=(7, 7),
+                            ksize_close: tuple=(9, 9)) -> np.ndarray:
+    """
+    Computes the binary mask of the detected Page from the probabilities outputed by network
+    :param probs: array in range [0, 1] of shape HxWx2
+    :param threshold: threshold between [0 and 1], if negative Otsu's adaptive threshold will be used
+    :param ksize_open: size of kernel for morphological opening
+    :param ksize_close: size of kernel for morphological closing
+    :return: binary mask
+    """
+    probs = probs[:, :, 1]
+    if threshold < 0:  # Otsu's thresholding
+        probs = np.uint8(probs * 255)
+        blur = cv2.GaussianBlur(probs, (5, 5), 0)
+        thresh_val, bin_img = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+        mask = np.uint8(bin_img / 255)
+    else:
+        mask = probs > threshold
+    mask = cv2.morphologyEx((mask.astype(np.uint8) * 255), cv2.MORPH_OPEN, kernel=np.ones(ksize_open))
+    mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel=np.ones(ksize_close))
+
+    result = mask / 255
+
+    return result
+
+
+def format_quad_to_string(quad):
+    """
+
+    :param quad:
+    :return:
+    """
+    s = ''
+    for corner in quad:
+        s += '{},{},'.format(corner[0], corner[1])
+    return s[:-1]
+
+
+def find_page(img_filenames, dir_predictions, post_process_params, output_dir):
+    """
+
+    :param img_filenames:
+    :param dir_predictions:
+    :param post_process_params:
+    :param output_dir:
+    :return:
+    """
+
+    # Write the corners points into a .txt file
+    with open(os.path.join(output_dir, 'pages.txt'), 'w') as f:
+        for filename in tqdm(img_filenames, 'Post-processing'):
+            orig_img = imread(filename, mode='RGB')
+            basename = os.path.basename(filename).split('.')[0]
+
+            filename_pred = os.path.join(dir_predictions, basename + '.npy')
+            pred = np.load(filename_pred)
+            # Make binary mask
+            page_bin = page_post_processing_fn(pred / np.max(pred), **post_process_params)
+
+            # Upscale to have full resolution image
+            target_shape = (orig_img.shape[1], orig_img.shape[0])
+            bin_upscaled = cv2.resize(np.uint8(page_bin), target_shape, interpolation=cv2.INTER_NEAREST)
+
+            # Find quadrilateral inclosing the page
+            pred_box = boxes_detection.find_box(np.uint8(bin_upscaled), mode='quadrilateral')
+
+            if pred_box is not None:
+                cv2.polylines(orig_img, [pred_box[:, None, :]], True, (0, 0, 255), thickness=15)
+            else:
+                print('No box found in {}'.format(filename))
+            imsave(os.path.join(output_dir, '{}_boxes.jpg'.format(basename)), orig_img)
+
+            f.write('{},{}\n'.format(filename, format_quad_to_string(pred_box)))
 
 # Export corners
 
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-o', '--output_dir', type=str, required=True,
+                        help='Folder containing the outputs (.npy predictions and visualization errors)')
+    parser.add_argument('--post_process_params', type=str, default=None,
+                        help='JSOn file containing the params for post-processing')
+    parser.add_argument('-b', '--box_mode', type=str, default='quadrilateral',
+                        help="Which type of box to use 'quadrilateral', 'min_rectangle', 'rectangle', 'no_box'")
+    parser.add_argument('--gpu', type=str, default='0', help='Which GPU to use')
+
+    export_models_dir = glob('demo/page_model/export/*')
+    export_models_dir.sort()
+    model_dir = export_models_dir[-1]
+
+    input_files = glob('demo/dataset/test/images/*')
+
+    output_dir = 'demo/processed_images'
+    os.makedirs(output_dir, exist_ok=True)
+
+    # with tempfile.TemporaryDirectory() as tmpdirname:
+    #     npy_directory = tmpdirname
+    #     # Load model and output probabilities masks
+    #     predict_on_set(input_files, model_dir, npy_directory)
+    #
+    #     npy_files = glob(os.path.join(npy_directory, '*.npy'))
+    #     find_page(input_files, npy_directory, post_process_params, output_dir, mode=args.get('box_mode'))
