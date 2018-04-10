1e54def669efa15d7b2705fdca02eca0354363b1
soliveir <sofia.oliveiraares@epfl.ch>
2018-04-10 10:43:30 +0200

    setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(name='dh_segment',
+      version='0.1',
+      url='https://github.com/dhlab-epfl/dhSegment',
+      packages=['dh_segment'],
+      zip_safe=False)
