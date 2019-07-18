ac1be7c73cdfcfb187d130bf2a513ecd9fb70898
soliveir <sofia.oliveiraares@epfl.ch>
2019-07-18 11:09:10 +0200

    updated cbad mask generation

--- exps/cbad/utils.py
+++ exps/cbad/utils.py
@@ -5,20 +5,26 @@ import cv2
 import numpy as np
 from imageio import imread, imsave
 from tqdm import tqdm
-from dh_segment.post_processing import PAGE
+from dh_segment.io import PAGE
 
 TARGET_HEIGHT = 1100
 DRAWING_COLOR_BASELINES = (255, 0, 0)
-DRAWING_COLOR_POINTS = (0, 255, 0)
+DRAWING_COLOR_LINES = (0, 255, 0)
+DRAWING_COLOR_POINTS = (0, 0, 255)
 
 RANDOM_SEED = 0
 np.random.seed(RANDOM_SEED)
 
 
 def get_page_filename(image_filename: str) -> str:
-    return os.path.join(os.path.dirname(image_filename),
-                        'page',
-                        '{}.xml'.format(os.path.basename(image_filename)[:-4]))
+    page_filename = os.path.join(os.path.dirname(image_filename),
+                                 'page',
+                                 '{}.xml'.format(os.path.basename(image_filename)[:-4]))
+
+    if os.path.exists(page_filename):
+        return page_filename
+    else:
+        raise FileNotFoundError
 
 
 def get_image_label_basename(image_filename: str) -> str:
@@ -39,56 +45,74 @@ def save_and_resize(img: np.array, filename: str, size=None, nearest: bool=False
         imsave(filename, img)
 
 
-def annotate_one_page(image_filename: str, output_dir: str, size: int, endpoints: bool=False,
-                      line_thickness: int=10, diameter_endpoint: int=20) -> None:
+def annotate_one_page(image_filename: str,
+                      output_dir: str,
+                      size: int,
+                      draw_baselines: bool=True,
+                      draw_lines: bool=False,
+                      draw_endpoints: bool=False,
+                      line_thickness: int=10,
+                      diameter_endpoint: int=20) -> None:
+
     page_filename = get_page_filename(image_filename)
     page = PAGE.parse_file(page_filename)
     text_lines = [tl for tr in page.text_regions for tl in tr.text_lines]
     img = imread(image_filename, pilmode='RGB')
     gt = np.zeros_like(img)
 
-    if not endpoints:
-        gt = cv2.polylines(gt,
-                           [(PAGE.Point.list_to_cv2poly(tl.baseline)[:, 0, :])[:, None, :] for tl in text_lines],
-                           isClosed=False, color=DRAWING_COLOR_BASELINES,
-                           thickness=int(line_thickness * (gt.shape[0] / TARGET_HEIGHT)))
+    if draw_baselines:
+        gt_baselines = np.zeros_like(img[:, :, 0])
+        gt_baselines = cv2.polylines(gt_baselines,
+                                     [PAGE.Point.list_to_cv2poly(tl.baseline) for tl in
+                                      text_lines],
+                                     isClosed=False, color=255,
+                                     thickness=int(line_thickness * (gt_baselines.shape[0] / TARGET_HEIGHT)))
+        gt[:, :, np.argmax(DRAWING_COLOR_BASELINES)] = gt_baselines
 
-    else:
+    if draw_lines:
         gt_lines = np.zeros_like(img[:, :, 0])
-        gt_lines = cv2.polylines(gt_lines,
-                                 [(PAGE.Point.list_to_cv2poly(tl.baseline)[:, 0, :])[:, None, :] for tl in text_lines],
-                                 isClosed=False, color=255,
-                                 thickness=int(line_thickness * (gt_lines.shape[0] / TARGET_HEIGHT)))
+        for tl in text_lines:
+            gt_lines = cv2.fillPoly(gt_lines,
+                                    [PAGE.Point.list_to_cv2poly(tl.coords)],
+                                    color=255)
+        gt[:, :, np.argmax(DRAWING_COLOR_LINES)] = gt_lines
 
+    if draw_endpoints:
         gt_points = np.zeros_like(img[:, :, 0])
         for tl in text_lines:
             try:
                 gt_points = cv2.circle(gt_points, (tl.baseline[0].x, tl.baseline[0].y),
-                                       radius=int((diameter_endpoint/2 * (gt_points.shape[0]/TARGET_HEIGHT))),
+                                       radius=int((diameter_endpoint / 2 * (gt_points.shape[0] / TARGET_HEIGHT))),
                                        color=255, thickness=-1)
                 gt_points = cv2.circle(gt_points, (tl.baseline[-1].x, tl.baseline[-1].y),
-                                       radius=int((diameter_endpoint/2 * (gt_points.shape[0]/TARGET_HEIGHT))),
+                                       radius=int((diameter_endpoint / 2 * (gt_points.shape[0] / TARGET_HEIGHT))),
                                        color=255, thickness=-1)
             except IndexError:
                 print('Length of baseline is {}'.format(len(tl.baseline)))
-
-        gt[:, :, np.argmax(DRAWING_COLOR_BASELINES)] = gt_lines
         gt[:, :, np.argmax(DRAWING_COLOR_POINTS)] = gt_points
 
-    save_and_resize(img, os.path.join(output_dir, 'images', '{}.jpg'.format(get_image_label_basename(image_filename))),
-                    size=size)
-    save_and_resize(gt, os.path.join(output_dir, 'labels', '{}.png'.format(get_image_label_basename(image_filename))),
+    image_label_basename = get_image_label_basename(image_filename)
+    save_and_resize(img, os.path.join(output_dir, 'images', '{}.jpg'.format(image_label_basename)), size=size)
+    save_and_resize(gt, os.path.join(output_dir, 'labels', '{}.png'.format(image_label_basename)),
                     size=size, nearest=True)
-    shutil.copy(page_filename, os.path.join(output_dir, 'gt', '{}.xml'.format(get_image_label_basename(image_filename))))
+    shutil.copy(page_filename, os.path.join(output_dir, 'gt', '{}.xml'.format(image_label_basename)))
 
 
-def cbad_set_generator(input_dir: str, output_dir: str, img_size: int, line_thickness: int=4,
-                           draw_endpoints: bool=False, circle_thickness: int =20):
+def cbad_set_generator(input_dir: str,
+                       output_dir: str,
+                       img_size: int,
+                       draw_baselines: bool=True,
+                       draw_lines: bool=False,
+                       line_thickness: int=4,
+                       draw_endpoints: bool=False,
+                       circle_thickness: int =20):
     """
 
     :param input_dir: Input directory containing images and PAGE files
     :param output_dir: Output directory to save images and labels
     :param img_size: Size of the resized image (# pixels)
+    :param draw_baselines: Draws the baselines (boolean)
+    :param draw_lines: Draws the polygon's lines (boolean)
     :param line_thickness: Thickness of annotated baseline
     :param draw_endpoints: Predict beginning and end of baselines (True, False)
     :param circle_thickness: Diameter of annotated start/end points
@@ -103,7 +127,9 @@ def cbad_set_generator(input_dir: str, output_dir: str, img_size: int, line_thic
     os.makedirs(os.path.join('{}'.format(output_dir), 'labels'))
     os.makedirs(os.path.join('{}'.format(output_dir), 'gt'))
     for image_filename in tqdm(image_filenames_list):
-        annotate_one_page(image_filename, output_dir, img_size, draw_endpoints, line_thickness, circle_thickness)
+        annotate_one_page(image_filename, output_dir, img_size, draw_baselines=draw_baselines,
+                          draw_lines=draw_lines, line_thickness=line_thickness,
+                          draw_endpoints=draw_endpoints, diameter_endpoint=circle_thickness)
 
     if draw_endpoints:
         classes = np.stack([(0, 0, 0), DRAWING_COLOR_BASELINES, DRAWING_COLOR_POINTS])
